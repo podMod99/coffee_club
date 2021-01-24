413d079be296e6876f253001d281dfb61239ed8c
podMod99 <Felix.butcher@gmx.com>
2021-01-24 11:23:07 +0100

    bug fixing

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -78,7 +78,7 @@ const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
     );
 
   if (error) {
-    console.log(error);
+    console.log('Checkout.jsx error message', error);
     Confirmation = () => (
       <>
         <Typography variant='h5'>Error: {error}</Typography>
